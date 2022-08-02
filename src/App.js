7cd513e19152a41eec71743b273f789d68852ed1
Charlie Mendoza <charlie.mendoza1337@gmail.com>
2022-08-01 20:32:30 -0400

    Layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import { Component } from "react";
 import Productos from './components/Productos'
+import Layout from './components/Layout'
 class App extends Component {
   state = {
     productos: [
       {name: 'Tomate', price:1500, img:'/productos/tomate.jpg'},
       {name: 'Arbejas', price:2000, img:'/productos/arbejas.jpg'},
       {name: 'Lechuga', price:500, img:'/productos/lechuga.jpg'},
-    ]
+    ] 
   }
   render(){
     return(
       <div>
-        <Productos
-          agregarAlCarro={() => console.log('No hace nada')}
-          productos={this.state.productos}
-        />
-        <p></p>
+        <Layout>
+          <Productos
+            agregarAlCarro={() => console.log('No hace nada')}
+            productos={this.state.productos}
+          />
+        </Layout>
       </div>
     )
   }

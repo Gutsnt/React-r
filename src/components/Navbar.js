926e6e8ed8a6194a674b31e4276fc1aab13e927e
Charlie Mendoza <charlie.mendoza1337@gmail.com>
2022-08-11 23:03:57 -0400

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,10 @@ const styles = {
 		flexDirection:'row',
 		alignItems:'center',
 		height:'100px',
+		justifyContent:'space-between',
+		position:'relative',
+		padding:'0 50px',
+		boxShadow:'0 2px 3px rgb(0,0,0,0.1)'
 	}
 }
 class Navbar extends Component {

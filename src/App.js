1c0a7c1c10e5842d9a5d912a0437df64ed797e7f
Charlie Mendoza <charlie.mendoza1337@gmail.com>
2022-08-30 04:00:19 -0400

    "controlled"

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+const App = () => {
+    const [value,setValue] = useState('')
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+    console.log(value)
+    return (
+        <div>
+            <input type="text" name="normal" value={value} onChange={handleChange} />
+        </div>
+    )
+}
+
+export default App
